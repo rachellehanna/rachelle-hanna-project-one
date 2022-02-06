a6454a1ba816f3b9126f22b1209dca8f293f5f6e
Rachelle Hanna <ha.rachelle@gmail.com>
2022-02-06 13:00:33 -0400

    Add comment feature

---
+++ script.js
@@ -0,0 +1,29 @@
+//set up event listener on post comment button
+//grab references to inputs
+//grab values from inputs
+
+const button = document.querySelector(".blogForm button");
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+    const inputName = document.getElementById("name");
+    const inputEmail = document.getElementById("email");
+    const inputComment = document.getElementById("comment");
+
+    const name = inputName.value;
+    const email = inputEmail.value;
+    const comment = inputComment.value;
+
+    //we take the strings and create element
+
+    const commentContainerDiv = document.createElement("div");
+    commentContainerDiv.classList.add("commentContainer");
+    const newContent = document.createTextNode(`${name} says ${comment}`);
+    commentContainerDiv.appendChild(newContent);
+
+    const commentsContainer = document.querySelector(".commentsContainer");
+    commentsContainer.appendChild(commentContainerDiv);
+
+    inputName.value = "";
+    inputEmail.value = "";
+    inputComment.value = "";
+});
